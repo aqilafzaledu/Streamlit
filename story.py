28682c26aa1b6486524bdd517909952e2de24fa6
aqilafzaledu <aqilafzaledu@gmail.com>
2024-10-01 08:02:31 +0000

    add some comment

--- story.py
+++ story.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from openai import OpenAI
 
-
+#need to change it to st.secrets is for stremlit.
 my_secret = os.st.secrets['OPENAI_API_KEY']
 #my_secret = os.environ['OPENAI_API_KEY']
 client = OpenAI(api_key=my_secret)
